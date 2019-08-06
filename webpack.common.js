317d2ef84600bfc188692b8413a2c28e17d8582c
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2019-08-06 16:58:32 -0300

    Configures babel/preset-env to to compile against the current node version.
    Fixes “TypeError: class constructors must be invoked with 'new’” error.

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,13 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                { "targets": { "node": "current" } }
+                            ],
+                            '@babel/preset-react'
+                        ],
                         plugins: ['@babel/plugin-proposal-object-rest-spread']
                     }
                 }

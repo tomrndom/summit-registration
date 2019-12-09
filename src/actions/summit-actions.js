0635a5bdff0a4291cbeb0eb583ea005551a78f36
Tomás Castillo <tcastilloboireau@gmail.com>
2019-12-09 19:20:34 -0300

    custom error handler for summit search

--- src/actions/summit-actions.js
+++ src/actions/summit-actions.js
@@ -22,6 +22,7 @@ import {
     showMessage,
     showSuccessMessage,
 } from 'openstack-uicore-foundation/lib/methods';
+import Swal from 'sweetalert2';
 
 import { LOGOUT_USER } from "openstack-uicore-foundation/lib/actions";
 
@@ -51,7 +52,7 @@ export const getSummitBySlug = (slug, updateSummit) => (dispatch, getState) => {
         dispatch(startLoading()),
         createAction(GET_SUMMIT_BY_SLUG),
         `${window.API_BASE_URL}/api/public/v1/summits/all/${slug}`,
-        authErrorHandler
+        customErrorHandler
     )(params)(dispatch).then((payload) => {
           if(updateSummit) {
             dispatch(createAction(SELECT_SUMMIT)(payload.response, false));
@@ -201,3 +202,63 @@ export const selectSummitById = (id) => (dispatch, getState) => {
 
 }
 
+
+export const customErrorHandler = (err, res) => (dispatch, state) => {
+  let code = err.status;
+  let msg = '';
+
+  dispatch(stopLoading());
+
+  switch (code) {
+      case 403:
+          let error_message = {
+              title: 'ERROR',
+              html: T.translate("errors.user_not_authz"),
+              type: 'error'
+          };
+
+          dispatch(showMessage( error_message, initLogOut ));
+          break;
+      case 401:
+          let currentLocation = window.location.pathname;
+          let clearing_session_state = window.clearing_session_state || false;
+
+          dispatch({
+              type: CLEAR_SESSION_STATE,
+              payload: {}
+          });
+
+          if(!clearing_session_state) {
+              window.clearing_session_state = true;
+              console.log('authErrorHandler 401 - re login');
+              doLogin(currentLocation);
+          }
+          break;
+      // case 404:
+      //     msg = "";
+
+      //     if (err.response.body && err.response.body.message) msg = err.response.body.message;
+      //     else if (err.response.error && err.response.error.message) msg = err.response.error.message;
+      //     else msg = err.message;
+
+      //     Swal.fire("Not Found", msg, "warning");
+
+      //     break;
+      case 412:
+          for (var [key, value] of Object.entries(err.response.body.errors)) {
+              if (isNaN(key)) {
+                  msg += key + ': ';
+              }
+
+              msg += value + '<br>';
+          }
+          Swal.fire("Validation error", msg, "warning");
+          dispatch({
+              type: VALIDATE,
+              payload: {errors: err.response.body.errors}
+          });
+          break;
+      default:
+          // Swal.fire("ERROR", T.translate("errors.server_error"), "error");
+  }
+}
